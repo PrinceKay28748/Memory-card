bb328fb1ca5f3346554b8a776d16b321e2fbd6ac
PrinceKay28748 <pkay28748@gmail.com>
2026-02-18 01:18:01 +0000

    header added

--- memory-card/src/frontpage.jsx
+++ memory-card/src/frontpage.jsx
@@ -1,26 +1,8 @@
-import { useState, useEffect } from "react";
-import "./frontpage.css";
-
-export default function FrontPage() {
-  const [currentScore, setCurrentScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-
+export default function Header() {
   return (
-    <div >
-    
-    <div className="header">
-      <h1>Welcome to the Memory Card Game!</h1>
-
-      <div>
-        <p>Current Score: {currentScore}</p>
-        <p>Best Score: {bestScore}</p>
-      </div>
-    </div>
-
-      <div className="maindiv">
-        Get points by clicking on matching cards.Do not click on the same card
-        twice!
-      </div>
-    </div>
+    <header className="header">
+      <h1 className="header-title">Memory Card Game</h1>
+      <p className="header-subtitle">Test your memory skills by finding matching pairs of cards!</p>
+    </header>
   );
 }
